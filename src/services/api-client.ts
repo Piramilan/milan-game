acb43b6e9eef1ba2987ae346f8211ec10212b6dd
MilanDev98 <132041534+PiramilanDev@users.noreply.github.com>
2023-08-20 22:17:06 +0530

    React Milan Games Hub

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "419c647fe13d408297b07f1c021e05ee",
+    key: process.env.REACT_APP_GAME_API_KEY, //Add Your API Key Here,
   },
 });
